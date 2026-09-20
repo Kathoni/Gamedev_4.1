2487585921857c43c8790126b84a86efeec35279
Kathoni <thrusherkeys@gmail.com>
2026-09-20 16:38:09 +0300

    add implementation of RockPaperScissors game

---
+++ RockPaperScissors.java
@@ -0,0 +1,52 @@
+package com.embu.basicgames;
+import java.util.Random;
+import java.util.Scanner;
+public class RockPaperScissors {
+public static void main(String[] args) {
+Scanner s= new Scanner(System.in);
+while (true) {
+String[] rps = { "r", "p", "s" };
+String computerMove = rps[new Random().nextInt(rps.length)];
+String playerMove;
+while (true) {
+System.out.println("Please enter your move (r, p, or s)");
+playerMove = s.nextLine();
+if (playerMove.equals("r") || playerMove.equals("p") || playerMove.equals("s")) {
+break;
+} else {
+System.out.println(playerMove + " is not a valid move.");
+
+}
+}
+ System.out.println("Computer played: " + computerMove);
+ if (playerMove.equals(computerMove)) {
+ System.out.println("The game was a tie!");
+ } else if (playerMove.equals("r")) {
+ if (computerMove.equals("p")) {
+System.out.println("You lose!");
+ } else if (computerMove.equals("s")) {
+System.out.println("You win!");
+ }
+ }
+else if (playerMove.equals("p")) {
+if (computerMove.equals("r")) {
+ System.out.println("You win!");
+ } else if (computerMove.equals("s")) {
+ System.out.println("You lose!");
+}
+ }else if (playerMove.equals("s")) {
+ if (computerMove.equals("p")) {
+System.out.println("You win!");
+ } else if (computerMove.equals("r")) {
+System.out.println("You lose!");
+ }
+ }
+System.out.println("Play again? (y/n)");
+String playAgain = s.nextLine();
+if (!playAgain.equals("y")) {
+break;
+}
+ }
+ s.close();
+ }
+}
