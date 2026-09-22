1f2d1a1ef79be0eb5f82cce9af66c387648a7c2c
Kathoni <thrusherkeys@gmail.com>
2026-09-22 21:25:31 +0300

    fix formatting of main method declaration in RockPaperScissors.java

--- RockPaperScissors.java
+++ RockPaperScissors.java
@@ -2,7 +2,8 @@
 import java.util.Random;
 import java.util.Scanner;
 public class RockPaperScissors {
-public static void main(String[] args) {
+public static void main(String[] args) 
+        {
   Scanner s= new Scanner(System.in);
     while (true) {
     String[] rps = { "r", "p", "s" };
